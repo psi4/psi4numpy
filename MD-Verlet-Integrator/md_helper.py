07889b1299565bb029c9e9c69f5e13ea144db878
Leonardo Cunha <ldac3@gatech.edu>
2017-05-24 21:49:37 -0300

    Updated Docs for md_helper

--- MD-Verlet-Integrator/md_helper.py
+++ MD-Verlet-Integrator/md_helper.py
@@ -15,30 +15,51 @@ import psi4
 import numpy as np
 import os
 
-# Function: md_trajectories
-# Create .xyz file with MD Trajectories for all points
-# Arguments: 
-#       max_md_steps (integer): Number of MD steps calculated
+# Converting Atomic Mass Unit (amu) to atomic units (au)
+amu2au = 1822.8884850
+
 def md_trajectories(max_md_step):
+    """ Creates single .xyz file with MD Trajectories with all points.
+        
+        Parameters:
+        ----------
+            int max_md_steps -- Number of MD steps calculated
+
+        Returns:
+        -------
+            file md_trajectories.xyz -- File containing all the trajectory points for visualization
+    """
+
     with open('md_trajectories.xyz','w') as outfile:
       for i in range(1,max_md_step+1):
         with open('md_step_'+str(i)+'.xyz')as infile:
             outfile.write(infile.read())
     os.system('rm md_step*')
 
-# Function: integrator
-# Select the type of integration algorithm to propagate the trajectories
-# Only velocity Verlet implemented (date: 05/23/17 - LAC)
-# Arguments:
-#       int_alg (string): Integrator Algorith to be used
-#           a) veloc_verlet: Use Velocity Verlet Algorithm
-#       timestep (float): Time step to be used on the MD trajectory propagation step
-#       pos (array (natom,3)): Array with old positions/trajectories
-#       veloc (array (natom,3)): Array with old velocities
-#       accel (array (natom,3)): Array with old accelerations
-#       molec (Psi4 molecule): Psi4 molecule object to be used
-#       grad_method (string): Method to be used to calculate energies and forces
 def integrator(int_alg,timestep,pos,veloc,accel,molec,grad_method):
+    """ Selects the type of integration algorithm to propagate the trajectories
+        Only velocity Verlet implemented (date: 05/23/17 - LAC)
+
+        Parameters:
+        ----------
+            string int_alg --  Integrator Algorith to be used
+                Options:
+                a) veloc_verlet --  Use Velocity Verlet Algorithm
+            float timestep -- Time step to be used on the MD trajectory propagation step
+            array pos --  Numpy array (natoms,3) with old positions/trajectories
+            array veloc --  Numpy array (natoms,3) with old velocities
+            array accel -- Numpy array (natoms,3) with old accelerations
+            psi4_object molec --  Psi4 molecule object to be used
+            string grad_method: Method to be used to calculate energies and forces
+
+        Returns:
+        -------
+            array pos_new -- Numpy array (natoms,3) with the updated positions
+            array vel_new -- Numpy array (natoms,3) with the updated velocities
+            array accel_new -- Numpy array (natoms,3) with the updated accelerations
+            float E -- Updated energy of the system
+    """
+
     natoms = molec.natom()
     atom_mass = np.asarray([molec.mass(atom) for atom in range(natoms)])*amu2au
 
@@ -52,12 +73,20 @@ def integrator(int_alg,timestep,pos,veloc,accel,molec,grad_method):
         vel_new += 0*5*timestep*accel_new
     return pos_new,vel_new,accel_new,E
 
-# Function: get_forces
-# Select the method (QC or FF) to be used to calculate energies and forces for the system
-# Only Psi4 supported methods implemented (date: 05/23/17 - LAC)
-# Arguments:
-#       grad_method (string): Method to be used to calculate forces and energies
 def get_forces(grad_method):
+    """ Selects the method (QC or FF) to be used to calculate energies and forces for the system
+        Only Psi4 supported methods implemented (date: 05/23/17 - LAC)
+    
+        Parameters:
+        ----------
+            string grad_method -- Method to be used to calculate forces and energies
+
+        Returns:
+        -------
+            float E -- Energy of the system
+            array force -- Numpy array (natoms,3) containing the forces acting on each atom of the system
+    """
+
     E,wfn = psi4.energy(grad_method,return_wfn=True)
     force = -np.asarray(psi4.gradient(grad_method,ref_wfn=wfn))
     return E,force
--- MD-Verlet-Integrator/md_prog.py
+++ MD-Verlet-Integrator/md_prog.py
@@ -16,7 +16,7 @@ import numpy as np
 import psi4
 import os
 import sys
-sys.path.append('../')
+sys.path.append('./')
 import md_helper
 
 psi4.core.set_output_file('md_output.dat',False)
@@ -32,7 +32,7 @@ amu2au = 1822.8884850
 
 #MD Options
 timestep =  5                       # Time step for each iteration in time atomic units
-max_md_step = 10000                 # Number of MD iterations
+max_md_step = 100                 # Number of MD iterations
 veloc0 = np.zeros((2,3))            # Numpy array (natoms,3) with inital velocities
 trajec = True                       # Boolean: Save all trajectories in a single xyz file (md_trajectories)  for visualization
 grad_method = 'hf/3-21g'            # Method (QC with basis set) for energy and gradient
@@ -41,39 +41,39 @@ opt    = False                      # Optimize geometry using F=ma
 
 # Molecular Dynamics Main Program
     
-    #Get initial positions,velocities, accelerations and forces
-    energy,forces = md_helper.get_forces(grad_method)
-    geom = np.asarray(molec.geometry())
-    natoms = molec.natom()
-    atom_mass = np.asarray([molec.mass(atom) for atom in range(natoms)])*amu2au
-    if opt:
-        veloc = np.zeros((natoms,3))
-    else:
-        veloc = veloc0
-    accel = forces/(atom_mass.reshape((natoms,1)))
-    
-    # MD LOOP
-    pos = geom
-    # Saving energies of each iteration on md_energy file
-    md_energy = open('md_energy.dat','w')
-    md_energy.write('File with the energy of each MD trajectory point\n\n')
-    md_energy.write('Trajectory Number\tEnergy (Hartree)\n')
-    md_energy.write('{0:>3d}\t\t\t{1:10.8f}\n'.format(1,energy))
-    for i in range(1,max_md_step+1):
-        
-        # Saving energies and trajectory points
-        md_energy.write('{0:>3d}\t\t\t{1:10.8f}\n'.format(i,energy))
-        if trajec:
-            molec.save_xyz_file('md_step_'+str(i)+'.xyz',False)
+#Get initial positions,velocities, accelerations and forces
+energy,forces = md_helper.get_forces(grad_method)
+geom = np.asarray(molec.geometry())
+natoms = molec.natom()
+atom_mass = np.asarray([molec.mass(atom) for atom in range(natoms)])*amu2au
+if opt:
+    veloc = np.zeros((natoms,3))
+else:
+    veloc = veloc0
+accel = forces/(atom_mass.reshape((natoms,1)))
 
-        # Updating positions velocities and accelerations using Velocity Verlet Integrator
-        pos_new,vel_new,accel_new,energy_new = md_helper.integrator(int_alg,timestep,pos,veloc,molec,grad_method)
-        pos = pos_new
-        if (not opt):
-            veloc = vel_new
-        accel = accel_new
-        energy = energy_new
-    md_energy.close()
+# MD LOOP
+pos = geom
+# Saving energies of each iteration on md_energy file
+md_energy = open('md_energy.dat','w')
+md_energy.write('File with the energy of each MD trajectory point\n\n')
+md_energy.write('Trajectory Number\tEnergy (Hartree)\n')
+md_energy.write('{0:>3d}\t\t\t{1:10.8f}\n'.format(1,energy))
+for i in range(1,max_md_step+1):
+    
+    # Saving energies and trajectory points
+    md_energy.write('{0:>3d}\t\t\t{1:10.8f}\n'.format(i,energy))
     if trajec:
-        md_helper.md_trajectories(max_md_step)
-    print "Done with Molecular Dynamics Program!"
+        molec.save_xyz_file('md_step_'+str(i)+'.xyz',False)
+
+    # Updating positions velocities and accelerations using Velocity Verlet Integrator
+    pos_new,vel_new,accel_new,energy_new = md_helper.integrator(int_alg,timestep,pos,veloc,accel,molec,grad_method)
+    pos = pos_new
+    if (not opt):
+        veloc = vel_new
+    accel = accel_new
+    energy = energy_new
+md_energy.close()
+if trajec:
+    md_helper.md_trajectories(max_md_step)
+print "Done with Molecular Dynamics Program!"
