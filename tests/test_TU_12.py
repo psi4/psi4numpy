3302524148a7434039e711ea1abce29a5c47e3a4
Andy Simmonett <andy.simmonett@gmail.com>
2017-10-03 16:01:43 -0600

    Add MD basics tutorial, update testing

--- tests/test_TU_12.py
+++ tests/test_TU_12.py
@@ -2,10 +2,14 @@ from addons import *
 from utils import *
 
 
-tdir = 'Tutorials/12_Ewald_Summation'
-
+tdir = 'Tutorials/12_MD'
 
 @using_matplotlib
 def test_12a(workspace):
-    exe_scriptified_ipynb(workspace, tdir, '12a_ewald')
+    exe_scriptified_ipynb(workspace, tdir, '12a_basics')
+
+@using_matplotlib
+@using_scipy
+def test_12b(workspace):
+    exe_scriptified_ipynb(workspace, tdir, '12b_ewald')
 
