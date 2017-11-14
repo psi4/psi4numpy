6a50a3ec2b2fdf98e99d8b63e97e3780cb7daf25
Lori A. Burns <lori.burns@gmail.com>
2017-11-14 16:49:26 -0500

    Update tests TU_13

--- tests/test_TU_13.py
+++ tests/test_TU_13.py
@@ -4,17 +4,17 @@ from utils import *
 tdir = 'Tutorials/13_Geometry-Optimization'
 
 def test_13a(workspace):
-    exe_scriptified_ipynb(workspace, tdir, '13a_Internal-Coordinates-Bmatrix.ipynb')
+    exe_scriptified_ipynb(workspace, tdir, '13a_Internal-Coordinates-Bmatrix')
 
 def test_13b(workspace):
-    exe_scriptified_ipynb(workspace, tdir, '13b_Hessians.ipynb')
+    exe_scriptified_ipynb(workspace, tdir, '13b_Hessians')
 
 def test_13c(workspace):
-    exe_scriptified_ipynb(workspace, tdir, '13c_Hessians-updating.ipynb')
+    exe_scriptified_ipynb(workspace, tdir, '13c_Hessians-updating')
 
 def test_13d(workspace):
-    exe_scriptified_ipynb(workspace, tdir, '13d_Rational-Function-Optimization.ipynb')
+    exe_scriptified_ipynb(workspace, tdir, '13d_Rational-Function-Optimization')
 
 def test_13e(workspace):
-    exe_scriptified_ipynb(workspace, tdir, '13e_Step-Backtransformation.ipynb')
+    exe_scriptified_ipynb(workspace, tdir, '13e_Step-Backtransformation')
 
