4c690f80a46da5c14c9fd52fd506222b38a80cd4
Eric Berquist <eric.berquist@gmail.com>
2017-12-22 17:19:33 -0500

    Start the tutorial notebook.

--- tests/test_TU_06.py
+++ tests/test_TU_06.py
@@ -7,3 +7,7 @@ tdir = 'Tutorials/06_Molecular_Properties'
 
 def test_6a(workspace):
     exe_scriptified_ipynb(workspace, tdir, '6a_CP-SCF')
+
+
+def test_6b(workspace):
+    exe_scriptified_ipynb(workspace, tdir, '6b_first_hyperpolarizability')
