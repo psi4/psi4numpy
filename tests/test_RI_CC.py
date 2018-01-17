888fb12572defbfba1998f2f208cad43ae0c74d4
Andrew M. James <amjames2@vt.edu>
2018-01-17 12:26:25 -0500

    [EOM] Add EOM_CCSD ref imp to tests

--- tests/test_RI_CC.py
+++ tests/test_RI_CC.py
@@ -17,5 +17,9 @@ def test_CCSD_T(workspace):
     exe_py(workspace, tdir, 'CCSD_T')
 
 
+def test_EOM_CCSD(workspace):
+    exe_py(workspace, tdir+'/spin_free_CC','EOM_CCSD')
+
+
 #def test_TD_CCSD(workspace):
 #    exe_py(workspace, tdir, 'TD-CCSD')
