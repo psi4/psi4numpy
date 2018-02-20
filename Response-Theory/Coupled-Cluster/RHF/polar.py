5180453779b635d58f7602dc20e77135059665cc
Ashutosh Kumar <akumar1@vt.edu>
2018-02-19 22:44:58 -0500

    factorized the inhomogenous terms in Y equations, added more documentation

--- Response-Theory/Coupled-Cluster/RHF/polar.py
+++ Response-Theory/Coupled-Cluster/RHF/polar.py
@@ -9,6 +9,7 @@ from helper_ccpert import *
 
 np.set_printoptions(precision=15, linewidth=200, suppress=True)
 import psi4
+from psi4 import constants as pc
 
 psi4.set_memory(int(2e9), False)
 psi4.core.set_output_file('output.dat', False)
@@ -24,7 +25,6 @@ psi4.set_options({'basis': 'cc-pvdz'})
 psi4.set_options({'scf_type': 'PK'})
 psi4.set_options({'d_convergence': 1e-10})
 psi4.set_options({'e_convergence': 1e-10})
-psi4.set_options({'r_convergence': 1e-10})
 rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
 print('RHF Final Energy                          % 16.10f\n' % rhf_e)
 
@@ -43,25 +43,27 @@ cchbar = HelperCCHbar(ccsd)
 cclambda = HelperCCLambda(ccsd,cchbar)
 cclambda.compute_lambda(r_conv=1e-10)
 
-# static polarizability
-omega = 0
+# nm into a.u    
+omega_nm = 589
+omega = (pc.c * pc.h * 1e9)/(pc.hartree2J * omega_nm)
 
 cart = ['X', 'Y', 'Z']
-mu = {}
+Mu = {}
 ccpert = {}
 polar_AB = {}
 
+dipole_array = ccsd.mints.ao_dipole()
 
 for i in range(0,3):
     string = "MU_" + cart[i]
-    mu[string] = np.einsum('uj,vi,uv', ccsd.npC, ccsd.npC, np.asarray(ccsd.mints.ao_dipole()[i]))
-    ccpert[string] = HelperCCPert(string, mu[string], ccsd, cchbar, cclambda, omega)
+    Mu[string] = np.einsum('uj,vi,uv', ccsd.npC, ccsd.npC, np.asarray(dipole_array[i]))
+    ccpert[string] = HelperCCPert(string, Mu[string], ccsd, cchbar, cclambda, omega)
     print('\nsolving right hand perturbed amplitudes for %s\n' % string)
     ccpert[string].solve('right', r_conv=1e-10)
     print('\nsolving left hand perturbed amplitudes for %s\n'% string)
     ccpert[string].solve('left', r_conv=1e-10)
 
-print("\nComputing <<Mu;Mu>_(%1.5lf) tensor." % omega)
+print("\nComputing <<Mu;Mu>_(%8.6lf au, %d nm) tensor." % (omega, omega_nm))
 
 for a in range(0,3):
     str_a = "MU_" + cart[a]
@@ -78,7 +80,7 @@ for a in range(0,3):
             polar_AB[ab] = 0.5*(polar_AB[ab] + polar_AB[ba])
             polar_AB[ba] = polar_AB[ab]    
 
-print('\n CCSD Dipole Polarizability Tensor (Symmetrized) at omega = %8.6lf a.u \n'% omega)
+print('\n CCSD Dipole Polarizability Tensor (Symmetrized) at omega = %8.6lf au, %d nm\n'% (omega, omega_nm))
 print("\t\t%s\t             %s\t                  %s\n" % (cart[0], cart[1], cart[2]))    
 for a in range(0,3):
     print(" %s %20.10lf %20.10lf %20.10lf\n" % ( cart[a], polar_AB[3*a+0], polar_AB[3*a+1], polar_AB[3*a+2]))    
@@ -87,7 +89,6 @@ trace = polar_AB[0] + polar_AB[4] + polar_AB[8]
 Isotropic_polar = trace/3.0
         
 # Comaprison with PSI4
-#psi4.set_options({'omega': [0, 'ev']})
-#psi4.properties('ccsd', properties=['polarizability'])
-#psi4.compare_values(Isotropic_polar, psi4.get_variable("CCSD DIPOLE POLARIZABILITY @ INF NM"),  3, "CCSD Isotropic Dipole Polarizability @ Inf nm") #TEST
-psi4.compare_values(Isotropic_polar, 6.756049543545,  9, "CCSD Isotropic Dipole Polarizability @ Inf nm") #TEST
+psi4.set_options({'omega': [589, 'nm']})
+psi4.properties('ccsd', properties=['polarizability'])
+psi4.compare_values(Isotropic_polar, psi4.get_variable("CCSD DIPOLE POLARIZABILITY @ 589NM"),  8, "CCSD Isotropic Dipole Polarizability @ 589 nm") #TEST
