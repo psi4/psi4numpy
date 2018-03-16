33f634a35bd313bfb6c76d60a05debb7523b24d5
alenaizan <alenaizana@gmail.com>
2018-03-16 18:21:04 -0400

    Update qm.py

--- Restrained-Electrostatic-Potential/qm.py
+++ Restrained-Electrostatic-Potential/qm.py
@@ -5,12 +5,13 @@ def psi4_esp(method, basis, molecule):
                basis: string of basis set
                molecule: an instance of psi4 Molecule class
         output:
-               wfn.oeprop.Vvals(): numpy array of QM electrostatic potential
+              numpy array of QM electrostatic potential
         Note: grid is available in grid.dat
      """
     import psi4
+    import numpy as np
     mol = psi4.geometry(molecule.create_psi4_string_from_molecule())
     psi4.set_options({'basis': basis})
     e, wfn = psi4.prop(method, properties=['GRID_ESP'], return_wfn=True)
     psi4.core.clean()
-    return wfn.oeprop.Vvals()
+    return np.loadtxt('grid_esp.dat')
