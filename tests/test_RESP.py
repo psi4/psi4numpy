329b812e7d32c1e661a9833d5da5dcb2a56dbc84
alenaizan <alenaizana@gmail.com>
2018-04-19 15:44:24 -0400

    update the directory in the test

--- tests/test_RESP.py
+++ tests/test_RESP.py
@@ -2,7 +2,7 @@ from addons import *
 from utils import *
 
 
-tdir = 'Restrained-Electrostatic-Potential'
+tdir = 'One-Electron-Property/Restrained-Electrostatic-Potential'
 
 def test_example(workspace):
     exe_py(workspace, tdir,'example')
