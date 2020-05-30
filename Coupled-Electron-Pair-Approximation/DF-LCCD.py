445a4d8ab92daba0064926655c2ec8ad3e056a7e
Jonathon Misiewicz <jm16657@uga.edu>
2020-05-30 17:19:53 -0400

    Style changes.

--- Coupled-Electron-Pair-Approximation/DF-LCCD.py
+++ Coupled-Electron-Pair-Approximation/DF-LCCD.py
@@ -29,7 +29,13 @@ compare_psi4 = True
 basis = "cc-pvdz"
 
 ### Setup
-psi4.set_options({"freeze_core": freeze_core, "scf_type": scf_type, "e_convergence": target_convergence + 1, "basis": basis, "cc_type": "df"})
+psi4.set_options({
+    "freeze_core": freeze_core,
+    "scf_type": scf_type,
+    "e_convergence": target_convergence + 1,
+    "basis": basis,
+    "cc_type": "df"
+})
 R, F = integrals_DF(mol)
 Fo = F["oo"].diagonal()
 Fv = F["vv"].diagonal()
@@ -40,29 +46,36 @@ nvir = len(Fv)
 
 ### Main Loop
 for i in range(1, maxiter + 1):
-    # Two Electron Terms
+
+    ### Two Electron Terms
+
     # r2 = g^ij_ab
-    temp = np.einsum("iaq, jbq -> ijab", R["ov"], R["ov"], optimize = True)
+    temp = np.einsum("iaq, jbq -> ijab", R["ov"], R["ov"], optimize=True)
     r2 = temp - temp.transpose((1, 0, 2, 3))
+
     # r2 += 0.5 g^AB_cd t^IJ_cd -> IJAB
     # Trying to do this computation by direct einsum leads to reassembling the full VVVV block of integrals.
     # We would rather not store a V^4 intermediate in memory if we can avoid it.
     # Accordingly, we loop over a V index.
     for A in range(nvir):
-        temp = np.einsum("cq, Bdq, IJcd -> IJB", R["vv"][A], R["vv"], t2, optimize = True)
+        temp = np.einsum("cq, Bdq, IJcd -> IJB", R["vv"][A], R["vv"], t2, optimize=True)
         r2[:, :, A, :] += temp
+
     # r2 += 0.5 g^kl_IJ t^kl_AB
     r2 += np.einsum("kIq, lJq, klAB -> IJAB", R["oo"], R["oo"], t2, optimize=True)
+
     # r2 += g^AkIc t^JkBc -> IJAB
     temp = np.einsum("IAq, kcq, JkBc -> IJAB", R["ov"], R["ov"], t2, optimize=True)
     temp -= np.einsum("kIq, Acq, JkBc -> IJAB", R["oo"], R["vv"], t2, optimize=True)
     r2 += temp + temp.transpose((1, 0, 3, 2)) - temp.transpose((0, 1, 3, 2)) - temp.transpose((1, 0, 2, 3))
-    # One Electron Terms. For canonical orbitals, this will reduce to -t2, after dividing by D
-    temp = - np.einsum("Ii, iJAB -> IJAB", F["oo"], t2, optimize=True)
+
+    ### One Electron Terms. For canonical orbitals, this will reduce to -t2, after dividing by D
+    temp = -np.einsum("Ii, iJAB -> IJAB", F["oo"], t2, optimize=True)
     r2 += temp - temp.transpose((1, 0, 2, 3))
-    temp = + np.einsum("aA, IJaB -> IJAB", F["vv"], t2, optimize=True)
+    temp = +np.einsum("aA, IJaB -> IJAB", F["vv"], t2, optimize=True)
     r2 += temp - temp.transpose((0, 1, 3, 2))
-    # Step
+
+    ### Step
     t2 += r2 / D
     t2 = dsd.diis(r2, t2)
     r_norm = np.linalg.norm(r2)
@@ -74,6 +87,7 @@ else:
     raise Exception("Equations did not converge.")
 
 if compare_psi4:
-   wfn = psi4.energy("lccd", return_wfn=True)[1]
-   # Change to wfn.variable when DFOCC variables are available on the wfn.
-   psi4.compare_values(psi4.variable("CURRENT CORRELATION ENERGY"), Elccd, target_convergence, "LCCD Energy")
+    wfn = psi4.energy("lccd", return_wfn=True)[1]
+
+    # Change to wfn.variable when DFOCC variables are available on the wfn.
+    psi4.compare_values(psi4.variable("CURRENT CORRELATION ENERGY"), Elccd, target_convergence, "LCCD Energy")
