4173e106f74d12722b1b3b218fcd1e890cfded67
Lori A. Burns <lori.burns@gmail.com>
2020-05-31 22:24:15 -0400

    Update LCCSD.py

--- Coupled-Electron-Pair-Approximation/LCCSD.py
+++ Coupled-Electron-Pair-Approximation/LCCSD.py
@@ -86,4 +86,4 @@ else:
 
 if compare_psi4:
    wfn = psi4.energy("lccsd", return_wfn=True)[1]
-   psi4.compare_values(wfn.variable("CURRENT CORRELATION ENERGY"), Elccsd, target_convergence, "LCCSD Energy")
+   psi4.compare_values(psi4.variable("CURRENT CORRELATION ENERGY"), Elccsd, target_convergence, "LCCSD Energy")
