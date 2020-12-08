c6a2759927578069e5e9b2ab9ac50113a7dbd90a
Kirk C Pearce <kcpearce@vt.edu>
2020-12-08 14:34:21 -0500

    Changes requested by DGAS

--- One-Electron-Property/IR-Intensities/intensities.py
+++ One-Electron-Property/IR-Intensities/intensities.py
@@ -24,52 +24,6 @@ psi4.core.set_output_file('output.dat', False)
 # Setup
 numpy_memory = 2 
 
-# Useful Constants
-psi_c = psi4.constants.get("Natural unit of velocity") * 100                                                    # speed of light in cm/s
-psi_na = psi4.constants.na                                                                                      # Avogdro's number
-psi_alpha = qcel.constants.get("fine-structure constant")                                                       # finite-structure constant
-
-# Unit Conversions
-#
-# Energy conversions
-hartree2joule = psi4.constants.get("Atomic unit of energy")                                                     # Eh -> J
-#
-# Distance conversions
-psi_bohr2m = psi4.constants.bohr2m                                                                              # bohr -> m
-sqbohr2sqmeter = psi_bohr2m ** 2                                                                                # bohr^2 -> m^2
-psi_bohr2angstroms = psi4.constants.bohr2angstroms                                                              # bohr -> Ang
-#
-# Mass conversions
-amu2kg = psi4.constants.get("Atomic mass constant")                                                             # amu -> kg
-psi_au2amu = psi4.constants.au2amu                                                                              # m_e -> amu
-#
-# Dipole moment type conversions
-psi_dipmom_au2debye = psi4.constants.dipmom_au2debye                                                            # e a0 -> D (C m)
-conv_dip_grad_au2DAamu = (psi_dipmom_au2debye / psi_bohr2angstroms) * (1 / np.sqrt(psi_au2amu))                 # (e a0 / a0)/(m_e^(1/2)) -> (D/A)/(amu^(1/2))
-conv_cgs = 6.46047502185 * (10**(-36))                                                                          # (e a0)^2 -> (esu * cm)^2
-#
-# IR frequency conversions
-omega2nu = 1./(psi_c*2*np.pi)                                                                                   # omega (s^-1)  -> nu (cm^-1)
-psi4_hartree2wavenumbers = psi4.constants.hartree2wavenumbers                                                   # Eh -> cm^-1
-conv_ir_au2DAamu = conv_dip_grad_au2DAamu ** 2                                                                  # (e a0 / a0)^2/m_e -> (D/A)^2/amu
-conv_freq_au2wavenumbers = np.sqrt((1 / psi_au2amu) * hartree2joule / (sqbohr2sqmeter * amu2kg)) * omega2nu     # (Eh / (bohr^2 m_e)) -> cm^-1
-conv_freq_wavenumbers2hartree = 1 / psi4_hartree2wavenumbers                                                    # cm^-1 -> Eh
-
-# Convert IR intensities from (D/A)^2/amu to km/mol:
-# (thanks to Zack Glick!)
-#
-# Conversion factor for taking IR intensities from ((D/A)^2 / amu) to a.u. ((e a0 / a0)^2 / me):
-conv_kmmol = (1 / conv_dip_grad_au2DAamu) ** 2
-# Multiply by (1 / (4 pi eps_0)) in a.u. (1 / (e^2 / a0 Eh)) to get to units of (Eh a0 / me)
-# Multiply by (Na pi / 3 c^2) in a.u. (Na = mol^-1; c = Eh / alpha^2 me) to get to units of a0/mol
-conv_kmmol *= psi_na * np.pi * (1 / 3) * psi_alpha**2
-# Convert to units of km/mol
-conv_kmmol *= psi_bohr2m * (1 / 1000)
-#
-conv_ir_DAamu2kmmol = conv_kmmol                                                                                # (D/A)^2/amu -> km/mol
-
-
-
 mol = psi4.geometry("""
 C  -1.2602962432       0.0000000000       0.0000000000
 C   1.2602962432       0.0000000000       0.0000000000
@@ -95,7 +49,7 @@ e, wfn = psi4.frequencies('scf', return_wfn=True)
 natoms = mol.natom()
 
 # Get array of atomic masses in a.u.
-masses = np.array([mol.mass(i) * (1 / psi_au2amu) for i in range(mol.natom())])
+masses = np.array([mol.mass(i) * psi4.constants.conversion_factor("amu", "atomic_unit_of_mass") for i in range(mol.natom())])
 
 # Get Hessian from wavefunction
 Hess = np.asarray(wfn.hessian())
@@ -104,70 +58,81 @@ print("\nMolecular Hessian (a.u.):\n", Hess)
 # Mass weight Hessian
 M = np.diag(1 / np.sqrt(np.repeat(masses, 3))) # Matrix for mass weighting
 mH = M.T.dot(Hess).dot(M) 
-#print("\nMass-weighted Hessian (a.u.):\n", mH)
-#print("\nMass-weighted Hessian (hartree/(bohr^2 amu)):\n", mH * (1 / psi_au2amu))
+print("\nMass-weighted Hessian (hartree/(bohr^2 amu)):\n", mH * psi4.constants.conversion_factor("amu", "atomic_unit_of_mass"))
 
 # Mass-weighted normal modes from Hessian
 k2, Lxm = np.linalg.eigh(mH) 
-#print("\nEigenvalues (a.u.):\n", k2) 
-#print("\nEigenvectors (unitless):\n", Lxm) 
-print("\nEigenvalues (hartree/(bohr^2 amu)):\n", k2 * (1 / psi_au2amu)) 
-#print("\nEigenvalues (J/kg*m^2 = s^-2):\n", k2 * (1 / psi_au2amu) * hartree2joule / (sqbohr2sqmeter * amu2kg)) 
+print("\nEigenvalues (hartree/(bohr^2 amu)):\n", k2 * psi4.constants.conversion_factor("hartree/(bohr^2 * atomic_unit_of_mass)", "hartree/(bohr^2 * amu)"))
+print("\nEigenvalues (J/kg*m^2 = s^-2):\n", k2 * psi4.constants.conversion_factor("hartree/(bohr^2 * atomic_unit_of_mass)", "s^-2"))
+print("\nEigenvectors (unitless):\n", Lxm) 
 
+# Calculate normal modes
 normal_modes = []
 mode = 3 * natoms - 1
 print("\nNormal Modes (cm^-1):")
 while mode >= 6:
     if k2[mode] >= 0.0:
         normal_modes.append(np.sqrt(k2[mode]))
-        print("%.2f" % (np.sqrt(k2[mode]) * conv_freq_au2wavenumbers))
+        print("%.2f" % (np.sqrt(k2[mode]) * psi4.constants.conversion_factor("hartree^(1/2)/(bohr * atomic_unit_of_mass^(1/2))", "cm^-1") / (2 * np.pi)))
     else:
         normal_modes.append(np.sqrt(abs(k2[mode])))
-        print("%.2fi" % (np.sqrt(abs(k2[mode])) * conv_freq_au2wavenumbers))
+        print("%.2fi" % (np.sqrt(abs(k2[mode])) * psi4.constants.conversion_factor("hartree^(1/2)/(bohr * atomic_unit_of_mass^(1/2))", "cm^-1") / (2 * np.pi)))
     mode -= 1
 
 # Test frequencies
 psi_freq = wfn.frequency_analysis['omega'].data[6:]
 psi_freq = np.flip(psi_freq, 0)
 for i in range(len(normal_modes)):
-    psi4.compare_values(psi_freq[i], normal_modes[i] * conv_freq_au2wavenumbers, 6, "FREQ-TEST")
+    psi4.compare_values(psi_freq[i], normal_modes[i] * psi4.constants.conversion_factor("hartree^(1/2)/(bohr * atomic_unit_of_mass^(1/2))", "cm^-1") / (2 * np.pi), 6, "FREQ-TEST")
 
 # Read in dipole derivatives
 dipder = wfn.variables().get("CURRENT DIPOLE GRADIENT", None)
 dipder = np.asarray(dipder)
-#print("\nDipole Derivatives (a.u.):\n", dipder)
-print("\nDipole Derivatives (D/A):\n", dipder * (psi_dipmom_au2debye / psi_bohr2angstroms))
+print("\nDipole Derivatives (D/A):\n", dipder * psi4.constants.conversion_factor("e * bohr / bohr", "debye / angstrom"))
 
 # Un-mass-weight the eigenvectors (now have units of m_e^(-1/2))
 Lx = M.dot(Lxm) 
 
 # Normal Coordinates transformation matrix
 S = np.flip(Lx, 1)[:,:len(normal_modes)]
-#print("\nNormal Coordinates transformation matrix (a.u.):\n", S)
-#print("\nNormal Coordinates transformation matrix (amu**-(1/2)):\n", S * (1 / np.sqrt(psi_au2amu)))
+print("\nNormal Coordinates transformation matrix (amu**-(1/2)):\n", S * psi4.constants.conversion_factor("atomic_unit_of_mass^(-1/2)", "amu^(-1/2)"))
 
 # Transform dipole derivatives to normal coordinates
 dip_grad = np.einsum('ij,jk->ik', dipder.T, S, optimize=True)
-print("\nDipole Gradient in Normal Coordinate Basis (D/(A*amu**(1/2))):\n", dip_grad * conv_dip_grad_au2DAamu)
+print("\nDipole Gradient in Normal Coordinate Basis ((D/A)/amu^(1/2)):\n", dip_grad * psi4.constants.conversion_factor("(e * bohr)/(bohr * atomic_unit_of_mass^(1/2))", "debye / (angstrom * amu^(1/2))"))
+
+# Conversion factor for (D/A)^2/amu to km/mol:
+# (thanks to Zack Glick!)
+#
+psi_na = psi4.constants.na # Avogdro's number
+psi_alpha = qcel.constants.get("fine-structure constant") # finite-structure constant
+#
+# Conversion factor for taking IR intensities from ((D/A)^2 / amu) to a.u. ((e a0 / a0)^2 / me):
+conv_kmmol = psi4.constants.conversion_factor("debye^2 / (angstrom^2 * amu)", "(e^2 * bohr^2)/(bohr^2 * atomic_unit_of_mass)")
+# Multiply by (1 / (4 pi eps_0)) in a.u. (1 / (e^2 / a0 Eh)) to get to units of (Eh a0 / me)
+conv_kmmol *= psi4.constants.conversion_factor("(e^2 * bohr^2)/(bohr^2 * atomic_unit_of_mass) * (1 / (e^2 / (bohr * hartree)))", "hartree * bohr / atomic_unit_of_mass")
+# Multiply by (Na pi / 3 c^2) in a.u. (Na = mol^-1; c = Eh / alpha^2 me) to get to units of a0/mol
+conv_kmmol *= psi_na * np.pi * (1 / 3) * psi_alpha**2
+# Convert to units of km/mol
+conv_kmmol *= psi4.constants.conversion_factor("bohr / mol", "km / mol")
+#
+conv_ir_DAamu2kmmol = conv_kmmol # (D/A)^2/amu -> km/mol
 
 # Compute IR Intensities
 IR_ints = np.zeros(len(normal_modes))
 for i in range(3):
     for j in range(len(normal_modes)):
         IR_ints[j] += dip_grad[i][j] * dip_grad[i][j]
-#print("\nIR Intensities (a.u.):\n", IR_ints)
-#print("\nIR Intensities ((D/A)^2/amu):\n", IR_ints * conv_ir_au2DAamu)
-#print("\nIR Intensities km/mol:\n", IR_ints * conv_ir_au2DAamu * conv_ir_DAamu2kmmol)
 
 print("\n\nVibrational Frequencies and IR Intensities:\n------------------------------------------\n")
 print("  mode        frequency               IR intensity\n========================================================")
 print("           cm-1      hartrees      km/mol  (D/A)**2/amu \n--------------------------------------------------------")
 for i in range(len(normal_modes)):
-    print("  %3d    %7.2f     %8.6f     %7.3f      %6.4f" % (i + 1, normal_modes[i] * conv_freq_au2wavenumbers, normal_modes[i] * conv_freq_au2wavenumbers * conv_freq_wavenumbers2hartree, IR_ints[i] * conv_ir_au2DAamu * conv_ir_DAamu2kmmol, IR_ints[i] * conv_ir_au2DAamu))
+    print("  %3d    %7.2f     %8.6f     %7.3f      %6.4f" % (i + 1, normal_modes[i] * psi4.constants.conversion_factor("hartree^(1/2)/(bohr * atomic_unit_of_mass^(1/2))", "cm^-1") / (2 * np.pi), normal_modes[i] * psi4.constants.conversion_factor("hartree^(1/2)/(bohr * atomic_unit_of_mass^(1/2))", "hartree") / (2 * np.pi), IR_ints[i] * psi4.constants.conversion_factor("(e^2 * bohr^2)/(bohr^2 * atomic_unit_of_mass)", "debye^2 / (angstrom^2 * amu)") * conv_ir_DAamu2kmmol, IR_ints[i] * psi4.constants.conversion_factor("(e^2 * bohr^2)/(bohr^2 * atomic_unit_of_mass)", "debye^2 / (angstrom^2 * amu)")))
 
 # Test IR intensities
 psi_irints = wfn.frequency_analysis["IR_intensity"].data[6:]
 psi_irints = np.flip(psi_irints, 0)
 for i in range(len(normal_modes)):
-    psi4.compare_values(psi_irints[i], IR_ints[i] * conv_ir_au2DAamu * conv_ir_DAamu2kmmol, 6, "IR-INTS-TEST")
+    psi4.compare_values(psi_irints[i], IR_ints[i] * psi4.constants.conversion_factor("(e^2 * bohr^2)/(bohr^2 * atomic_unit_of_mass)", "debye^2 / (angstrom^2 * amu)") * conv_ir_DAamu2kmmol, 6, "IR-INTS-TEST")
 
