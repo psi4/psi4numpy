cf16c09ae2f78529fee6919077ffe73ded581f1b
FoleyLab <foleyj10@wpunj.edu>
2021-06-18 15:57:40 -0400

    added arbitrary component dipole and quadrupole terms to core Hamiltonian in CQED_RHF

--- CQED-Methods/CQED_RHF.py
+++ CQED-Methods/CQED_RHF.py
@@ -38,9 +38,10 @@ Mg 1 1.5
 #symmetry c1
 #""")
 
-psi4.set_options({'basis': 'cc-pvdz',
-                  'scf_type': 'pk',
-                  'e_convergence': 1e-8})
+psi4.set_options({'basis':        'sto-3g',
+                  'scf_type':     'pk',
+                  'reference':    'rhf',
+                  'save_jk': True})
 
 # Set defaults
 maxiter = 40
@@ -48,13 +49,23 @@ E_conv = 1.0E-6
 D_conv = 1.0E-3
 
 # electric field parameter
-lam_z = 1e-3
+Ex = 0
+Ey = 1e-5
+Ez = 1e-3
+lam = np.array([Ex, Ey, Ez])
 
 # Integral generation from Psi4's MintsHelper
-wfn = psi4.core.Wavefunction.build(mol, psi4.core.get_global_option('BASIS'))
-t = time.time()
+#wfn = psi4.core.Wavefunction.build(mol, psi4.core.get_global_option('BASIS'))
+#t = time.time()
+#mints = psi4.core.MintsHelper(wfn.basisset())
+scf_e, wfn = psi4.energy('scf', return_wfn=True)
 mints = psi4.core.MintsHelper(wfn.basisset())
+
+# ==> Nuclear Repulsion Energy <==
+E_nuc = mol.nuclear_repulsion_energy()
+
 S = np.asarray(mints.ao_overlap())
+
 
 # Get nbf and ndocc for closed shell molecules
 nbf = S.shape[0]
@@ -79,20 +90,76 @@ V = np.asarray(mints.ao_potential())
 T = np.asarray(mints.ao_kinetic())
 I = np.asarray(mints.ao_eri())
 
-# Extra terms for Pauli-Fierz Hamiltonian... assuming electric field
-# is only z-polarized for now!
+# Transformation vectors from ordinary SCF
+C = np.asarray(wfn.Ca())
 
-# dipole matrix
-mu_z = np.asarray(mints.ao_dipole()[2])
-# quadrupole matrix
-Q_z = np.asarray(mints.ao_quadrupole()[2])
+# number of doubly occupied orbitals
+ndocc   = wfn.nalpha()
 
-print('\nTotal time taken for integrals: %.3f seconds.' % (time.time() - t))
+# Extra terms for Pauli-Fierz Hamiltonian
+# nuclear dipole
+mu_nuc_x = mol.nuclear_dipole()[0]
+mu_nuc_y = mol.nuclear_dipole()[1]
+mu_nuc_z = mol.nuclear_dipole()[2]
+
+# dipole arrays in AO basis
+mu_x_ao = np.asarray(mints.ao_dipole()[0])
+mu_y_ao = np.asarray(mints.ao_dipole()[1])
+my_z_ao = np.asarray(mints.ao_dipole()[2])
+
+# transform dipole array to MO basis from ordinary RHF (no photon)
+mu_x = np.dot(C.T, mu_x_ao).dot(C)
+mu_y = np.dot(C.T, mu_y_ao).dot(C)
+mu_z = np.dot(C.T, mu_z_ao).dot(C)
+
+# compute components of electronic dipole moment <mu> from ordinary RHF (no photon)
+mu_exp_x = 0.
+mu_exp_y = 0.
+mu_exp_z = 0.
+for i in range(0, ndocc):
+    # double because this is only alpha terms!
+    mu_exp_x += 2*mu_x[i,i]
+    mu_exp_y += 2*mu_y[i,i]
+    mu_exp_z += 2*mu_z[i,i]
+
+# We need to carry around the electric field dotted into the nuclear dipole moment
+# and the electric field dotted into the RHF electronic dipole expectation value...
+# so let's compute them here!
+
+# lambda . mu_nuc
+l_dot_mu_nuc = lam[0] * mu_nuc_x + lam[1] * mu_nuc_y + lam[2] * mu_nuc_z
+l_dot_mu_el = lam[0] * mu_exp_x + lam[1] * mu_exp_y + lam[2] * mu_exp_z
+l_dot_mu = l_dot_mu_nuc + l_dot_mu_el
+
+# quadrupole arrays
+Q_xx = np.asarray(mints.ao_quadrupole()[0])
+Q_xy = np.asarray(mints.ao_quadrupole()[1])
+Q_xz = np.asarray(mints.ao_quadrupole()[2])
+Q_yy = np.asarray(mints.ao_quadrupole()[3])
+Q_yz = np.asarray(mints.ao_quadrupole()[4])
+Q_zz = np.asarray(mints.ao_quadrupole()[5])
+
+#print('\nTotal time taken for integrals: %.3f seconds.' % (time.time() - t))
 t = time.time()
 
 # Build H_core: [Szabo:1996] Eqn. 3.153, pp. 141
-# plus quadrupole terms from Pauli-Fierz Hamiltonian
-H = T + V + 0.5 * lam_z * Q_z
+# plus terms from Pauli-Fierz Hamiltonian
+# ordinary H_core
+H_0 = T + V 
+
+# Pauli-Fierz 1-e quadrupole terms
+Q_PF = lam[0]*lam[0] * Q_xx 
+Q_PF += lam[1]*lam[1] * Q_yy
+Q_PF += lam[2]*lam[2] * Q_zz
+Q_PF += 2 * lam[0]*lam[1] * Q_xy
+Q_PF += 2 * lam[0]*lam[2] * Q_xz
+Q_PF += 2 * lam[1]*lam[2] * Q_yz
+
+# Pauli-Fierz 1-e dipole terms scaled by l . <mu>
+d_PF = 2 * l_dot_mu * lam[0] * mu_x
+d_PF += 2 * l_dot_mu * lam[1] * mu_y
+d_PF += 2 * l_dot_mu * lam[2] * mu_z
+H = H_0 + Q_PF + d_PF
 
 # Orthogonalizer A = S^(-1/2) using Psi4's matrix power.
 A = mints.ao_overlap()
